b3d9d0ba2c458063e955ae08a699b996c7d5fd40
Catherine Östlund <catherine.oestlund@gmail.com>
2023-01-27 10:40:12 +0100

    refactor TodoList and add css

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import './TodoList.css';
 import {Todo} from '../utils/types'
 import {todos} from '../utils/state';
 import TodoCard from './TodoCard';
 
 const TodoList = () => {
+  const nrTodosLeft: number = todos.value.filter(todo => todo.done !== true).length;
+  const completedTodos: number = todos.value.length - nrTodosLeft;
+  
   return (
-    <section>
-      <h1>{todos.value.length > 0 ? 'All the things to do' : 'Everything is done for now'}</h1>
-      {todos.value && todos.value.map((todo:Todo) => (
-       <TodoCard todo={todo}/>
-        ),
-      )}
-    </section>
+    <>
+    <header className="list__header">
+      <h2>{todos.value.length > 0 ? 'All the things to do' : 'Everything is done for now'}</h2>
+      {completedTodos > 0 && <p className="list__todo-count"> Completed todos: {completedTodos}</p>}
+      {nrTodosLeft > 0 && <p className="list__todo-count"> Remaing things on list: {nrTodosLeft}</p>}
+    </header>
+      <section key="todoList" className="todo-list">
+        {todos.value && todos.value.map((todo:Todo, i) => (
+        <TodoCard todo={todo} key={todo.id}/>
+          ),
+        )}
+      </section>
+    </>
   )
 }
 
