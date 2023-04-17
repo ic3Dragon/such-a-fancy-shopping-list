9f99a2d50e0d32eecd990c5dbee65503d3991c6d
Catherine Östlund <catherine.oestlund@gmail.com>
2023-04-17 14:31:00 +0200

    add: styling for grid option

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
   <nav className="main__navBar">
     <button className="nav__button button" onClick={()=> display.value = 'flex'}>Use Flex</button>
-    <button className="nav__button button" hidden onClick={()=> display.value = 'grid'}>Use Grid</button>
+    <button className="nav__button button" onClick={()=> display.value = 'grid'}>Use Grid</button>
     <button className="nav__button button" onClick={()=> display.value = 'block'}>Use JavaScript</button>
   </nav>  
 )}
